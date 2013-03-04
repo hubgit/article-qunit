b0e6c948de5bc13720d3e11da73f50bfebc732e9
Alf Eaton <alf@hubmed.org>
2013-03-04 18:16:15 +0000

    loader

--- loader.js
+++ loader.js
@@ -1,11 +1,14 @@
 (function() {
-	var base = document.getElementById("article-qunit-loader").href.replace(/\/[^\/]*$/, "");
+	var base = document.getElementById("article-qunit-loader").src.replace(/\/[^\/]*$/, "");
 
-	$("<link/>", { rel: "stylesheet", href: base + "/vendor/qunit-git.css" }).appendTo("head");
+	$("<link/>", { rel: "stylesheet", href: base + "/vendor/qunit.css" }).appendTo("head");
 
 	var dependencies = [
-		base + "/qunit-git.js",
-		base + "/jquery-ui.js",
+		base + "/vendor/jquery.js",
+		base + "/vendor/jquery-ui.js",
+		base + "/vendor/qunit.js",
+		base + "/vendor/a.js",
+		base + "/vendor/microdata.js",
 	];
 
 	var i = 0;
